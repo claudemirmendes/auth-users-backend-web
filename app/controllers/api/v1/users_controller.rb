d8f39928290e591f31c001ccecfc8a33d09cd9a2
Claudemir <claudemir.avelino@vilaapps.com.br>
2023-09-02 14:47:02 -0300

    create validation role types and check if admin

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class UsersController < PrivateController
-    before_action :authenticate_admin, only: [:index, :show, :update, :destroy]
+    before_action :authenticate_admin!
     def index
       @users = User.all
       render json: @users
@@ -26,17 +26,39 @@ module Api
       @user.destroy
       head :no_content
     end
-  
-    private
 
+    def create
+      unless valid_role?(params[:role])
+        render json: { error: 'Invalid role type' }, status: :unprocessable_entity
+        return
+      end
+      @user = User.new(user_params)
+      @user.add_role(params[:role]) if params[:role].present?
   
-    def user_params
-      params.require(:user).permit(:email, :password, :role)
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
+  
     def authenticate_admin!
       unless current_user && current_user.has_role?(:admin)
         render json: { error: 'Unauthorized' }, status: :unauthorized
       end
+    end
+    private
+
+    def valid_role?(role)
+
+      valid_roles = ['admin', 'secretary', 'analyst', 'adminstrative','financial']
+
+
+      valid_roles.include?(role)
+    end
+  
+    def user_params
+      params.require(:user).permit(:email, :password, :role)
     end
   end
 end
