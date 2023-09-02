f872e0755edb90693b46c376cc05b9401f16db80
Claudemir <claudemir.avelino@vilaapps.com.br>
2023-09-02 15:59:32 -0300

    config

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Mount Rswag::Api::Engine and DeviseTokenAuth routes
   mount Rswag::Api::Engine => '/api-docs'
   mount_devise_token_auth_for 'User', at: 'auth'
-
+  root 'welcome#index'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: [:index, :show, :update, :destroy,:create]
